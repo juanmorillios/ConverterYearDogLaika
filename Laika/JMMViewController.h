c9533d6fee5c9f60e7f0d35e65955ad8d68608dd
Juan <juanmorillios@gmail.com>
2014-03-16 19:26:19 +0100

    Control Flow
    Control Flow

--- Laika/JMMViewController.h
+++ Laika/JMMViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface JMMViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *yearsLabel;
+@property (strong, nonatomic) IBOutlet UITextField *yearsTextField;
+@property (strong, nonatomic) IBOutlet UILabel *realYearsLabel;
+
+- (IBAction)convertToDogButtonPressed:(UIButton *)sender;
+- (IBAction)convertToRealYearsPressed:(UIButton *)sender;
 
 @end
--- Laika/JMMViewController.m
+++ Laika/JMMViewController.m
@@ -10,13 +10,27 @@
 
 @interface JMMViewController ()
 
+
+
 @end
 
 @implementation JMMViewController
 
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+
+
+    
+
+    
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +40,38 @@
     // Dispose of any resources that can be recreated.
 }
 
+    
+
+    
+
+- (IBAction)convertToDogButtonPressed:(UIButton *)sender {
+    
+    int humanYears = [self.yearsTextField.text intValue];
+    
+    int dogYears = humanYears * 7;
+    
+    self.yearsLabel.text =  [ NSString stringWithFormat:@"%i", dogYears];
+    
+    [self.yearsTextField resignFirstResponder];
+}
+
+- (IBAction)convertToRealYearsPressed:(UIButton *)sender {
+    
+    int humanYears = [self.yearsTextField.text intValue];
+    
+    int dogYears;
+    
+    
+    
+    if (humanYears > 2) {
+        dogYears  = (10.5 *2) + ((humanYears -2) * 4);
+    }else {
+        dogYears = 10.5 * humanYears;
+    }
+    
+    self.realYearsLabel.text = [NSString stringWithFormat:@"%i", dogYears];
+    
+    [self.yearsTextField resignFirstResponder];
+    
+}
 @end
